49b84db5aadb95a1457a350256e59a84d90853f0
MANISH27D <57955594+MANISH27D@users.noreply.github.com>
2025-09-11 00:33:08 +0530

    Update evensum.java

--- evensum.java
+++ evensum.java
@@ -1,43 +1,23 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class EvenNumbersGenerator {
+public class EvenSum {
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Please provide a number as a command-line argument.");
-            return;
-        }
+        // Pre-defined variable num
+        int num = 10; // Change this value to test different scenarios
 
-        int num;
-        try {
-            num = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid number format. Please provide an integer.");
-            return;
-        }
-
-        if (num <= 0) {
-            System.out.println("Please provide a positive number.");
-            return;
-        }
-
-        List<Integer> evenNumbers = new ArrayList<>();
+        StringBuilder evenNumbers = new StringBuilder();
         int sum = 0;
 
         for (int i = 2; i <= num; i += 2) {
-            evenNumbers.add(i);
+            evenNumbers.append(i);
             sum += i;
+            if (i < num && (i + 2) <= num) { // Add comma only if there are more even numbers to follow
+                evenNumbers.append(",");
+            }
         }
 
-        String outputNumbers = evenNumbers.stream()
-                                         .map(String::valueOf)
-                                         .collect(Collectors.joining(","));
-
-        System.out.println(outputNumbers);
+        System.out.println(evenNumbers.toString());
         System.out.println(sum);
     }
 }
